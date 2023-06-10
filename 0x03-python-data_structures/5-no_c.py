cf8c7364d4891bdb146afa52f1bde50f58248a62
Hatimsel <selmun13@gmail.com>
2023-06-10 21:14:07 +0100

    can you c me now

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    length = len(my_string)
+    i = 0
+    while i < length:
+        if my_string[i] == 'C' or my_string[i] == 'c':
+            i += 1
+            continue
+        else:
+            print("{:s}".format(my_string[i]), end = "")
+            i += 1
