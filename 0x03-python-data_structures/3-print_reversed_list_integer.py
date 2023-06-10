5317b7d5891ecace86d3ba314955cf6a975fadba
Hatimsel <selmun13@gmail.com>
2023-06-10 19:59:27 +0100

    reverse a list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    length = len(my_list)
+    i = length - 1
+    while i >= 0:
+        print("{}".format(my_list[i]))
+        i -= 1
