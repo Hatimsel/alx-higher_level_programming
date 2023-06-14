6b87fcdb1ba41e2f1c120f68858a7f7b0c521631
Hatimsel <selmun13@gmail.com>
2023-06-14 20:41:04 +0100

    squaring matrix elements

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -8,7 +8,7 @@ def square_matrix_simple(matrix=[]):
         new_matrix.append(new_row)
 
     rows = len(new_matrix)
-    columns = len(new_matrix[0])
+    columns = len(new_matrix[0]) if rows > 0 else 0
 
     for i in range(rows):
         for j in range(columns):
