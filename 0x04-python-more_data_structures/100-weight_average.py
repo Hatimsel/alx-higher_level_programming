020b74836ced7befdaf1d8ea4a04fbd38c1dda29
Hatimsel <selmun13@gmail.com>
2023-06-16 16:22:59 +0100

    Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    x = []
+    wet = 0
+    j = 0
+    y = 0
+    if not my_list:
+        return None
+    for lst in my_list:
+        for i in range(len(lst)):
+            wet += lst[1]
+            x.append(lst[0] * lst[1])
+    for j in range(len(x)):
+        y += x[j]
+    y = float(y / wet)
+    return y
