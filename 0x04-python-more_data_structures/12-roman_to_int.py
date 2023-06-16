c5a10d03bfd1d56b8f9b3a9d7c94cac9eed5f44c
Hatimsel <selmun13@gmail.com>
2023-06-16 13:42:12 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
     prev_value = 0
     result = 0
-    if not roman_string or not str(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
     for char in reversed(roman_string):
         value = rom_dict[char]
