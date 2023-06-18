75904989edfdcf6b9e6bd37c07340f00fa750bfe
Hatimsel <selmun13@gmail.com>
2023-06-18 11:00:43 +0100

    cmd args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ length = len(argv)
 n = 1
 if __name__ == "__main__":
     if length == 1:
-        print("0 arguments")
+        print("0 arguments.")
     elif length == 2:
         print("1 argument:")
         print("1: {:s}".format(argv[1]))
