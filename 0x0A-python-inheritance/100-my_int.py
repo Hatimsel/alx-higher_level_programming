f99db1b6cffd359406cf242ff4de31e2c02e1642
Hatimsel <selmun13@gmail.com>
2023-07-11 16:42:05 +0100

    fixing 1

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""My integer"""
+
+
+class MyInt(int):
+    """a subclass of int"""
+    def invert(self):
+        if True:
+            return False
+        if False:
+            return True
