a5f4afbaea716b84e81d9e8b304b082ca80a3ef4
Hatimsel <selmun13@gmail.com>
2023-09-06 07:30:47 +0100

    tdd

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+say my name function
+returns an official format of a name
+"""
+
 
 def say_my_name(first_name, last_name=""):
+    """
+    ...
+    ...
+    ...
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
