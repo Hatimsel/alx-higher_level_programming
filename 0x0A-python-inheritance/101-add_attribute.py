e61b9baae98985a1699bb1c27a39db2fd8a6e850
Hatimsel <selmun13@gmail.com>
2023-09-11 19:25:02 +0100

    add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a function that adds a new attribute if possible
+"""
+
+
+def add_attribute(obj, usr, val):
+    """
+    adds a new attribute if possible
+    """
+    setattr(obj, usr, val)
