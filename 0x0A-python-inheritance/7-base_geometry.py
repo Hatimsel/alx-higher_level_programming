a4177131ce347d8b4a4c85f396e6203c72c3aeb5
Hatimsel <selmun13@gmail.com>
2023-09-11 14:12:37 +0100

    Rectangle class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,10 +12,7 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        self.name = name
         if type(value) != int:
-            raise TypeError('{} must be an integer'.format(self.name))
+            raise TypeError('{} must be an integer'.format(name))
         elif value <= 0:
-            raise ValueError('{} must be greater then 0'.format(self.name))
-        else:
-            self.value = value
+            raise ValueError('{} must be greater then 0'.format(name))
