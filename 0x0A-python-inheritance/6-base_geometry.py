fdcb33745a7653e97800cf9d0c873760cd72e03f
Hatimsel <selmun13@gmail.com>
2023-09-11 13:55:08 +0100

    area is not

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+empty class
+"""
+
+
+class BaseGeometry:
+    """
+    not empty anymore
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
