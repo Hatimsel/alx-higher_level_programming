069176570797c0f87646ec27167e61afd2fa97d2
Hatimsel <selmun13@gmail.com>
2023-09-11 12:20:56 +0100

    dir()

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,4 +5,7 @@ lookup function take an obj as a parameter and returns attrs and methods
 
 
 def lookup(obj):
+    """
+    returns a list of attrs and methods available
+    """
     return dir(obj)
