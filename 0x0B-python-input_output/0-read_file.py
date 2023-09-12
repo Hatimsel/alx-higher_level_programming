04cc3017d5c9fccc8795e38a015625fb2e9205bd
Hatimsel <selmun13@gmail.com>
2023-09-12 10:19:24 +0100

    end=

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     """
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-    print("{:s}".format(read_data))
+    print("{:s}".format(read_data), end='')
