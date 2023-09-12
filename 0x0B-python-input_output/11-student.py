58253dcc28350c444d5cf3c200bdd6a7001a93b4
Hatimsel <selmun13@gmail.com>
2023-09-12 18:11:27 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Student to json
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        class Instantiation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            if all(isinstance(item, str) for item in attrs):
+                dic = {i: self.__dict__[i] for i in attrs if hasattr(self, i)}
+                return dic
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        dic = self.__dict__
+        dic = {setattr(self, k, v) for k, v in json.items()}
+        # for key, value in json.items():
+        #     setattr(self, key, value)
