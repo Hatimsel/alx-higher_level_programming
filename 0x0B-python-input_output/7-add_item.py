f8b58ba5c58c4f87c9d1279cbb410cc813b49bda
Hatimsel <selmun13@gmail.com>
2023-09-12 17:09:20 +0100

    a script that load, add, and save to a json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """
 load, add, save
 """
-import json
+import os
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -11,10 +11,11 @@ argv = len(sys.argv)
 new_list = []
 filename = "add_item.json"
 
+if os.path.isfile(filename):
+    new_list = load_from_json_file(filename)
+
 for i in range(1, argv):
     new_list.append(str(sys.argv[i]))
 
 with open(filename, 'a', encoding="utf-8") as f:
     save_to_json_file(new_list, filename)
-
-load_from_json_file(filename)
