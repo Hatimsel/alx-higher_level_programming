480c684e50029bfe8069b0833408b2dab3ae187f
Hatimsel <selmun13@gmail.com>
2023-09-12 07:39:46 +0100

    equal rebelion

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+inverting equal operator with non-equal
+"""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
