36b256a64cbb29723464a8969dbf6dbafae9c054
Hatimsel <selmun13@gmail.com>
2023-09-13 07:48:03 +0100

    Search and Update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Search and Update
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        content = f.readlines()
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        for line in content:
+            f.write(line)
+            if (search_string + " ") in line:
+                f.write(new_string)
