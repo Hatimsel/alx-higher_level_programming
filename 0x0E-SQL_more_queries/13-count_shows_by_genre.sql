c39560f97a27b986e4235f8eb098ab8c1b52e442
Hatim Selmun <selmun13@gmail.com>
2023-10-18 19:51:54 +0100

    number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all genres contained in the database
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
