c3c31a107b7b351a3b897f7d2a604dc09bc38751
Hatim Selmun <selmun13@gmail.com>
2023-10-18 20:15:34 +0100

    fixing 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Read user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-SET GLOBAL validate_password.policy = LOW;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-SET GLOBAL validate_password.policy = MEDIUM;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
