994224d95219f23a2f30684ce4a466b457c2cf88
Hatim Selmun <selmun13@gmail.com>
2023-11-17 17:33:00 +0100

    filter states by user iput

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values
+in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Script should take 3 arguments")
+    else:
+        db = MySQLdb.connect(host='localhost', port=3306,
+                             user=sys.argv[1], passwd=sys.argv[2],
+                             db=sys.argv[3])
+        cursor = db.cursor()
+        query = (
+            "SELECT * FROM states\
+            WHERE name = %s\
+            ORDER BY id ASC")
+        cursor.execute(query, (sys.argv[4],))
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+        cursor.close()
+        db.close()
