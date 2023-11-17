dfe7ab2e86ad37fa3ab8e7d996b91bdc1fef9a46
Hatim Selmun <selmun13@gmail.com>
2023-11-17 17:09:42 +0100

    get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 4:
         print("Script should take 3 arguments")
     else:
-        db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        db = MySQLdb.connect(host='localhost', port=3306,
+                             user=sys.argv[1], passwd=sys.argv[2],
+                             db=sys.argv[3])
         cursor = db.cursor()
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
         result = cursor.fetchall()
