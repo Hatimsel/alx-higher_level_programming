98890deb35bcdba805f7f1454237a6b0787bfdaf
Hatim Selmun <selmun13@gmail.com>
2024-01-11 10:54:47 +0100

    filter states by name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Python script that lists all states
+with a name matches the argument from a db
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Usage: ./script username passwd db state's name")
+    else:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3],
+                             port=3306)
+        cur = db.cursor()
+        cur.execute("""
+        SELECT * FROM states WHERE states.name LIKE BINARY '{}'
+        ORDER BY states.id ASC
+        """.format(sys.argv[4]))
+        data = cur.fetchall()
+
+        for row in data:
+            print(row)
