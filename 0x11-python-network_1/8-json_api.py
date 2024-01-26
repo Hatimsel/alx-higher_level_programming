3dea6bc4ba09a156710e9bea042808a9b24a7add
Hatim Selmun <selmun13@gmail.com>
2024-01-26 17:48:18 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,11 +7,12 @@ import sys
 
 
 if __name__ == "__main__":
-    q = sys.argv[2] if not None else ""
-    r = requests.post(sys.argv[1], data={'q': q})
+    q = sys.argv[1] if not None else ""
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data={'q': q})
     try:
         data = r.json()
-        print(f'[{data['id']} {data['name']}]')
+        print(f"[{data['id']} {data['name']}]")
     except requests.exceptions.InvalidJSONError:
         print('Not a valid JSON')
     except requests.exceptions.JSONDecodeError:
